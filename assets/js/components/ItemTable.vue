573f3925a35ef83e9c71f2fc27c1ba8bb95cd7c8
Darren Black <d.black@federationtraining.edu.au>
2019-06-25 15:08:28 +1000

    More

--- assets/js/components/ItemTable.vue
+++ assets/js/components/ItemTable.vue
@@ -1,14 +1,42 @@
 <template>
     <div>
-        <b-form-group label-cols-sm="3" label="Filter">
-            <b-input-group>
-            <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
-            <b-input-group-append>
-                <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
-            </b-input-group-append>
-            </b-input-group>
-        </b-form-group>
-        <b-table  hover :items="items" :fields="fields" :filter="filter"></b-table>
+        <b-row class="my-1">
+            <b-col>
+                <b-form inline>
+                    <b-input-group>
+                        <b-form-input v-model="filter.keyword" placeholder="Filter"></b-form-input>
+                        <b-input-group-append>
+                            <b-button :disabled="!filter.keyword" @click="filter.keyword = ''">Clear</b-button>
+                        </b-input-group-append>
+                    </b-input-group>
+                    <b-form-checkbox v-model="filter.active" name="filter-active" switch class="mx-4">
+                        Active
+                    </b-form-checkbox>
+                    <b-pagination
+                        v-model="pagination.currentPage"
+                        :total-rows="pagination.totalRows"
+                        :per-page="pagination.perPage"
+                        :limit="pagination.limit"
+                        class="ml-auto my-0"
+                    >
+                    </b-pagination>
+                </b-form>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <b-table 
+                    responsive="md"
+                    hover
+                    :items="filteredItems"
+                    :fields="fields"
+                    sort-by="name"
+                    :per-page="pagination.perPage"
+                    :current-page="pagination.currentPage"
+                >
+                </b-table>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
@@ -17,6 +45,10 @@ export default {
     props: {
         items: Array
     },
+    mounted() {
+      //Set the initial number of items
+      this.pagination.totalRows = this.filteredItems.length
+    },
     data () {
         return ({
             fields: [
@@ -24,8 +56,33 @@ export default {
                 { key: 'role', sortable: true },
                 { key: 'code', sortable: false }
             ],
-            filter: ''
+            filter: {
+                keyword: '',
+                active: true
+            },
+            pagination: {
+                totalRows: 1,
+                currentPage: 1,
+                perPage: 10,
+                pageOptions: [5, 10, 15],
+                limit: 10
+            }
         })
+    },
+    computed: {
+        filteredItems() {
+            let keyword = this.filter.keyword.toLowerCase()
+            let filteredItems = this.items.filter((item) => {
+                return item.active === this.filter.active && (
+                    !this.filter.keyword
+                    || item.name.toLowerCase().includes(keyword)
+                    || item.role.toLowerCase().includes(keyword)
+                    || item.code.toLowerCase().includes(keyword)
+                )
+            })
+            this.pagination.totalRows = filteredItems.length;
+            return filteredItems;
+        }
     }
 }
 </script>
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -6,44 +6,313 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/test")
+ */
 class TestController extends AbstractController
 {
     /**
-     * @Route("/test")
+     * @Route("/other", name="test_other")
+     */
+    public function other()
+    {
+        return $this->render('test/other.html.twig');
+    }
+
+    /**
+     * @Route("/table", name="test_table")
      */
     public function table()
     {
-        return $this->render('test.html.twig', [
+        return $this->render('test/table.html.twig', [
             'items' => json_encode([
                 [
                     "name" => "Tino Areno",
                     "role" => "Software Engineer",
-                    "code" => "Always keeping it clean"
+                    "code" => "Always keeping it clean",
+                    "active" => true,
                 ],
                 [
                     "name" => "Bobby Slack",
                     "role" => "Software Engineer",
-                    "code" => "Always keeping it real"
+                    "code" => "Always keeping it real",
+                    "active" => true,
                 ],
                 [
                     "name" => "Someone Else",
                     "role" => "Database Guru",
-                    "code" => "Always keeping it right"
+                    "code" => "Always keeping it right",
+                    "active" => false,
                 ],
                 [
                     "name" => "Another Dude",
                     "role" => "Frontend Dev",
-                    "code" => "Always keeping it bright"
+                    "code" => "Always keeping it bright",
+                    "active" => true,
                 ],
                 [
                     "name" => "Harry Manga",
                     "role" => "Frontend Dev",
-                    "code" => "Always keeping it purple"
+                    "code" => "Always keeping it purple",
+                    "active" => false,
                 ],
                 [
                     "name" => "Uncle Bob",
                     "role" => "Team Leader",
-                    "code" => "Always keeping it steam"
+                    "code" => "Always keeping it steam",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Someone Areno",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it clean",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Another Slack",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it real",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Harry Else",
+                    "role" => "Database Guru",
+                    "code" => "Always keeping it right",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Uncle Dude",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it bright",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Tino Manga",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it purple",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Bobby Bob",
+                    "role" => "Team Leader",
+                    "code" => "Always keeping it steam",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Bobby Areno",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it clean",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Tino Slack",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it real",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Bobby Else",
+                    "role" => "Database Guru",
+                    "code" => "Always keeping it right",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Someone Dude",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it bright",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Another Manga",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it purple",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Harry Bob",
+                    "role" => "Team Leader",
+                    "code" => "Always keeping it steam",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Uncle Areno",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it clean",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Someone Slack",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it real",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Another Else",
+                    "role" => "Database Guru",
+                    "code" => "Always keeping it right",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Harry Dude",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it bright",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Uncle Manga",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it purple",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Tino Bob",
+                    "role" => "Team Leader",
+                    "code" => "Always keeping it steam",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Areno Tino",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it clean",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Slack Bobby",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it real",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Else Someone",
+                    "role" => "Database Guru",
+                    "code" => "Always keeping it right",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Dude Another",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it bright",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Manga Harry",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it purple",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Bob Uncle",
+                    "role" => "Team Leader",
+                    "code" => "Always keeping it steam",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Areno Someone",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it clean",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Slack Another",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it real",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Else Harry",
+                    "role" => "Database Guru",
+                    "code" => "Always keeping it right",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Dude Uncle",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it bright",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Manga Tino",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it purple",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Bob Bobby",
+                    "role" => "Team Leader",
+                    "code" => "Always keeping it steam",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Areno Bobby",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it clean",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Slack Tino",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it real",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Else Bobby",
+                    "role" => "Database Guru",
+                    "code" => "Always keeping it right",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Dude Someone",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it bright",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Manga Another",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it purple",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Bob Harry",
+                    "role" => "Team Leader",
+                    "code" => "Always keeping it steam",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Areno Uncle",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it clean",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Slack Someone",
+                    "role" => "Software Engineer",
+                    "code" => "Always keeping it real",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Else Another",
+                    "role" => "Database Guru",
+                    "code" => "Always keeping it right",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Dude Harry",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it bright",
+                    "active" => true,
+                ],
+                [
+                    "name" => "Manga Uncle",
+                    "role" => "Frontend Dev",
+                    "code" => "Always keeping it purple",
+                    "active" => false,
+                ],
+                [
+                    "name" => "Bob Tino",
+                    "role" => "Team Leader",
+                    "code" => "Always keeping it steam",
+                    "active" => true,
                 ]
             ])
         ]);
